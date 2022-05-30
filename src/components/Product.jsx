7bfa7084cd7cf6aec0db17a9d02fa5ddba1ba0c7
adityamuthal22 <adityamuthal.2208@gmail.com>
2022-05-30 17:10:56 +0530

    Code Added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import {
+  Box,
+  Heading,
+  Image,
+  Stack,
+  Tag,
+  TagLabel,
+  Text,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-const Product = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+const Product = ({ pData }) => {
+  console.log(pData);
   return (
-    <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
-      <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
-      </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
-    </Stack>
+    <div>
+      {Object.keys(pData).map((key, value) => (
+        <Stack data-cy="product">
+          <Image data-cy="product-image" />
+          <Text data-cy="product-category">{key[value]}</Text>
+          <Tag>
+            <TagLabel data-cy="product-gender"></TagLabel>
+          </Tag>
+          <Heading data-cy="product-title"></Heading>
+          <Box data-cy="product-price"></Box>
+        </Stack>
+      ))}
+    </div>
   );
 };
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import { Flex, Grid } from '@chakra-ui/react'
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import AddProduct from "./AddProduct";
+import Pagination from "./Pagination";
+import Product from "./Product";
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  const [pData, setPData] = useState({});
+  useEffect(() => {
+    axios.get("http://localhost:8080/products").then((res) => {
+      // console.log(res.data);
+      setPData(res.data);
+    });
+  }, []);
+  // console.log(pData);
 
   return (
     <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
+      <AddProduct />
+      <Grid>
+        <Product pData={pData} />
+      </Grid>
+      <Pagination />
     </Flex>
   );
 };
 
 export default Products;
+
